df6ab74cf388ba2073a8d059516f3995e768dce0
Harsh Chopra <harshchopra527@gmail.com>
2018-10-22 09:41:27 +0530

    Basics of threading using java

---
+++ threading.java
@@ -0,0 +1,29 @@
+/*public class threading {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+
+        System.out.println(t.getPriority());
+        System.out.println(t.getName());
+    }
+}*/
+class threading {
+
+    public static void main(String args[])
+
+    {
+
+        byte x = 64;
+
+        int i;
+
+        byte y;
+
+        i = x << 2;
+
+        y = (byte) (x << 2);
+
+        System.out.print(i + " " + y);
+
+    }
+
+}
